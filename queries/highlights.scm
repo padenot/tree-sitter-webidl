645c8b6001cad562dc37df66dc01e95a8ccc66d4
Paul Adenot <paul@paul.cx>
2025-08-05 17:04:45 +0200

    highlights

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,21 +1,6 @@
 ;; Highlights for WebIDL grammar
 
-;; (identifier) @variable
-;;
-;; (type) @type
-;;
-;; (enum_value) @string
-;;
-;; (attribute "attribute") @keyword
-;; (method) @function
-;;
-;; ("interface" "typedef" "enum") @keyword
-;;
-;; ("{" "}" ";" "," "(") @punctuation.delimiter
-;;
 (comment) @comment
-
-; highlights.scm
 
 ; Keywords
 "interface" @keyword
@@ -34,11 +19,21 @@
 "readonly" @keyword
 "required" @keyword
 "optional" @keyword
+"inherit" @keyword
+"stringifier" @keyword
+"getter" @keyword
+"setter" @keyword
+"deleter" @keyword
+"legacycaller" @keyword
+"async_iterable" @keyword
+"iterable" @keyword
+"maplike" @keyword
+"setlike" @keyword
+"unrestricted" @keyword
 
 
 ; Extended attribute identifiers
 (extended_attribute (identifier) @attribute)
-
 
 ; Types
 ((primitive_type) @type)
@@ -50,6 +45,9 @@
 (dictionary name: (identifier) @type)
 (enum name: (identifier) @type)
 (namespace name: (identifier) @type)
+(callback_interface name: (identifier) @type)
+(callback_constructor name: (identifier) @type)
+(mixin name: (identifier) @type)
 (field name: (identifier) @property)
 (operation name: (identifier) @function.method)
 (attribute name: (identifier) @property)
@@ -63,8 +61,14 @@
 ((number_literal) @number)
 ((string_literal) @string)
 
-
+; Special constructs
+(stringifier_attribute) @keyword
+(stringifier_operation) @keyword
+(getter_operation) @keyword
+(setter_operation) @keyword
+(deleter_operation) @keyword
+(legacycaller_operation) @keyword
 
 ; Punctuation
-["(" ")" "{" "}" "<" ">" ";" "," "=" "?"] @punctuation.delimiter
+["(" ")" "{" "}" "<" ">" ";" "," "=" "?" "[" "]" "*" "..."] @punctuation.delimiter
 
